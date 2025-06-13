2d4c05bbaa5d2d6a466acabe09033ef0a692ca67
Cristian Salomon <crisalgarol@gmail.com>
2025-06-13 12:20:03 -0600

    Adding constant definitions

---
+++ Sources/iCarouselView/Enums/iCarouselOption.swift
@@ -0,0 +1,25 @@
+//
+//  iCarouselOptions.swift
+//  CarouBeko
+//
+//  Created by Cristian Olmedo on 20/04/25.
+//
+
+import Foundation
+
+enum iCarouselOption {
+    case wrap
+    case showBackFaces
+    case offsetMultiplier
+    case visibleItems
+    case optionCount
+    case optionARC
+    case optionAngle
+    case optionRadius
+    case optionTilt
+    case optionSpacing
+    case optionFadeMin
+    case optionFadeMax
+    case optionFadeRange
+    case optionFadeMinAlpha
+}
---
+++ Sources/iCarouselView/Enums/iCarouselType.swift
@@ -0,0 +1,23 @@
+//
+//  iCarouselType.swift
+//  CarouBeko
+//
+//  Created by Cristian Olmedo on 20/04/25.
+//
+
+import Foundation
+
+enum iCarouselType: String, CaseIterable {
+    case linear
+    case rotary
+    case invertedRotary
+    case cylinder
+    case invertedCylinder
+    case wheel
+    case invertedWheel
+    case coverFlow
+    case coverFlowTwo
+    case timeMachine
+    case invertedTimeMachine
+    case custom
+}
